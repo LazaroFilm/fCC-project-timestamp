b187e4d86d3104da9b41d67095012ec0f5b16630
LazaroFilm <lazarofilm@gmail.com>
2020-11-15 19:26:30 +0000

    Done

--- server.js
+++ server.js
@@ -8,25 +8,51 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
+
+
+app.get(`/api/timestamp/:reqDate`, (req, res) => {
+  const { reqDate } = req.params;
+  let unix = 0
+  let utc = ""
+  if (Date.parse(reqDate)) { // if the date is in a readable time format
+    unix = Date.parse(reqDate);
+    utc = new Date(reqDate).toUTCString()
+    res.json({ unix, utc })
+  } else if (/^\d{13}$/.test(reqDate)) { // if the time is in unix format
+    unix = reqDate;
+    utc = new Date(parseInt(reqDate)).toUTCString()
+    res.json({ unix, utc })
+  } else {
+    res.json({ "error": "invalid Date" })
+  }
+})
+
+// empty parameter returns current time
+app.get(`/api/timestamp`, (req, res) => {
+  const now = new Date();
+  const unix = Date.parse(now);
+  const utc = now.toUTCString();
+  res.json({ unix, utc })
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
